ca4471049ecaa29d7bc81c84d73c81f9bba5737d
songanji <songanji@gmail.com>
2015-02-01 17:17:42 +0900

    WIKI domain add

--- src/main/java/com/libqa/domain/Wiki.groovy
+++ src/main/java/com/libqa/domain/Wiki.groovy
@@ -1,12 +1,14 @@
-package com.libqa.domain.wiki
+package com.libqa.domain
 
 import lombok.Data
 import lombok.extern.slf4j.Slf4j
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.OneToMany
 import javax.persistence.Table
 import javax.persistence.Temporal
 import javax.persistence.TemporalType
@@ -16,6 +18,10 @@ import javax.persistence.TemporalType
 @Table(name = "wiki")
 @Slf4j
 class Wiki implements Serializable{
+
+
+    //@OneToMany(mappedBy = "wiki", fetch = FetchType.EAGER)
+    //private
 
     @Id
     @Column(name = "spaceId", nullable = false)
