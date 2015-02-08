534e1ae04a302a259c9bfdb53d1bb9f8adfd8314
yionlee <performizer@gmail.com>
2015-02-09 08:16:05 +0900

    한글처리

--- src/main/java/com/libqa/Application.java
+++ src/main/java/com/libqa/Application.java
@@ -3,9 +3,15 @@ package com.libqa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 /**
@@ -24,5 +30,18 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println("#### " + beanName);
         }
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
     }
 }
