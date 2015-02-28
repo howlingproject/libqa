6813ca88307c0e579ac6c951f7502272b68730be
yionlee <performizer@gmail.com>
2015-02-28 22:58:38 +0900

    응답클래스 추가

---
+++ src/main/java/com/libqa/application/ResponseData.java
@@ -0,0 +1,51 @@
+package com.libqa.application;
+
+import com.libqa.application.enums.StatusCodeEnum;
+
+import lombok.Data;
+
+/**
+ * 응답 결과를 반환한다.
+ * Created by yion on 2015. 2. 20..
+ */
+@Data
+public class ResponseData<T> {
+
+    private StatusCodeEnum StatusCode;
+    private String comment;
+    private T data;
+
+    public ResponseData(StatusCodeEnum code, String comment, T data) {
+        init(code, comment, data);
+    }
+
+    private void init(StatusCodeEnum code, String comment, T data) {
+        this.StatusCode = code;
+        this.comment = comment;
+        this.data = data;
+    }
+
+    public static <T> ResponseData<T> createResult(StatusCodeEnum code, String comment, T data) {
+        return new ResponseData<>(code, comment, data);
+    }
+
+    public static <T> ResponseData<T> createSuccessResult(T data) {
+        return new ResponseData<>(StatusCodeEnum.SUCCESS, StatusCodeEnum.SUCCESS.getComment(), data);
+    }
+
+    public ResponseData<T> setStatusCodeEnum(StatusCodeEnum code) {
+        this.StatusCode = code;
+        return this;
+    }
+
+    public ResponseData<T> setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
+
+    public ResponseData<T> setData(T data) {
+        this.data = data;
+        return this;
+    }
+
+}
---
+++ src/main/java/com/libqa/application/enums/StatusCodeEnum.java
@@ -0,0 +1,28 @@
+package com.libqa.application.enums;
+
+import lombok.Getter;
+
+/**
+ * Created by yion on 2015. 2. 20..
+ */
+public enum StatusCodeEnum {
+    FAIL(-1, "FAIL"),
+    NONE(0, "NA"),
+    SUCCESS(1, "SUCCESS"),
+    INVALID_STATUS(102, "잘못된 상태"),
+    INVALID_PARAMETER(900, "잘못된 파라미터"),
+    INTERNAL(990, "내부 오류"),
+    SYSTEM_ERROR(999, "시스템 오류");
+
+    @Getter
+    private final int code;
+
+    @Getter
+    private final String comment;
+
+    private StatusCodeEnum(final int code, final String comment) {
+        this.code = code;
+        this.comment = comment;
+    }
+
+}
