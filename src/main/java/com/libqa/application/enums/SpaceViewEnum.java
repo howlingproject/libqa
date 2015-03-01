45708235b3971be92ba4146c66da2f7f425c7d5a
yionlee <performizer@gmail.com>
2015-03-01 14:04:20 +0900

    view 파일 추가

--- src/main/java/com/libqa/application/enums/SpaceViewEnum.java
+++ src/main/java/com/libqa/application/enums/SpaceViewEnum.java
@@ -4,4 +4,7 @@ package com.libqa.application.enums;
  * Created by yion on 2015. 2. 1..
  */
 public enum SpaceViewEnum {
+    RIGHT,
+    LEFT,
+    CENTER
 }
--- src/main/java/com/libqa/web/controller/SpaceController.java
+++ src/main/java/com/libqa/web/controller/SpaceController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.libqa.application.enums.SpaceViewEnum;
 
 /**
  * Created by yion on 2015. 2. 8..
@@ -23,15 +24,13 @@ public class SpaceController {
     @RequestMapping("/space/form")
     public ModelAndView index(Model model) {
         log.debug("##############################");
-        ModelAndView mav = new ModelAndView("form");
+        log.debug("# message : {}", message);
+        log.debug("# SpaceViewEnum : {}", SpaceViewEnum.values());
+
+        ModelAndView mav = new ModelAndView("/space/form");
+        mav.addObject("SpaceViewEnum", SpaceViewEnum.values());
         mav.addObject("message", message);
         return mav;
     }
-    @RequestMapping("/space/form1")
-    public ModelAndView index1(Model model) {
-        log.info("##############################");
-        ModelAndView mav = new ModelAndView("space/form");
-        mav.addObject("message", message);
-        return mav;
-    }
+
 }
