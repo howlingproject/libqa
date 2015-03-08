9efb64bdfe4c079379344e3404a3e63247d6b91b
yionlee <performizer@gmail.com>
2015-03-08 13:28:18 +0900

    파일 임시저장 완성

--- src/main/java/com/libqa/application/util/FileUtil.java
+++ src/main/java/com/libqa/application/util/FileUtil.java
@@ -1,7 +1,13 @@
 package com.libqa.application.util;
 
+import com.libqa.application.dto.FileDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by yion on 2015. 3. 2..
@@ -9,21 +15,72 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 public class FileUtil {
 
+    public static String SEPARATOR = "/";
+
     /**
-     * 업로드가 허용되지 않는 파일일 경우 true 를 리턴한다.
-     * 확장자 만으로는 파일을 막을 수 없으므로 다른 방식의 보안 처리를 고려해야 한다.
+     * 확장자 체크
+     */
+    public static final String[] PERMITTED_EXTS = new String[] {
+            "DOC", "DOCX", "XLS", "XLSX", "PPT", "PPTX", "PPS", "HWP", "TXT", "PDF",
+            "ZIP", "ALZ", "JAR",  "AI", "PSD", "XMIND",
+            "AVI", "MOV", "MPG", "MPEG", "RM", "ASF", "WMV", "MP3", "MP4",
+            "BMP", "JPG", "JPEG", "GIF", "PNG", "FLA", "SWF", "HTM", "HTML" , "HTML",
+            "XML", "WAR", "JAR", "TAR.GZ", "GZ", "DMG", "GDOC", "GSHEET", "GSLIDES",
+            "DMG", "APK"
+    };
+
+
+
+    /**
+     * MultipartFile 정보를 추출하여 FileDto 에 저장한다.
+     * @param uploadfile
+     * @param localDir
+     * @return
+     * @throws Exception
+     */
+    public static FileDto extractFileInfo(MultipartFile uploadfile, String localDir) throws Exception {
+        FileDto fileDto = new FileDto();
+        String today = DateUtil.getToday();
+
+        log.info("## uploadfile : {}", uploadfile.getContentType());
+        log.info("## uploadfile : {}", uploadfile.getName());
+        log.info("## uploadfile : {}", uploadfile.getOriginalFilename());
+        log.info("## uploadfile : {}", uploadfile.getSize());
+        String[] fileType = uploadfile.getContentType().split(SEPARATOR);
+
+        String fileOriginalName = uploadfile.getOriginalFilename();
+        //업로드 파일 확장자 취득
+        int dotIndex = fileOriginalName.lastIndexOf('.');
+        String extendName = fileOriginalName.substring(dotIndex + 1);
+        String temp = String.valueOf(System.nanoTime());
+
+        //저장 파일명 (ex : 2015030214332087117027.jpg)
+        String saveFileName = today + temp + "." + extendName.toLowerCase();
+        double fileSize = uploadfile.getSize();
+        String uploadType = fileType[0];
+
+        fileDto.setFilePath(localDir);
+        fileDto.setFileSize(fileSize);
+        fileDto.setFileExtendType(extendName);
+        fileDto.setRealName(fileOriginalName);
+        fileDto.setSavedName(saveFileName);
+        fileDto.setUploadType(uploadType);
+        return fileDto;
+    }
+
+
+
+    /**
+     * 업로드 허용파일 목록
      * @param file
      */
-    public static void notAllowedFile(MultipartFile file) {
-        final String[] notAccess = {"jsp", "php", "asp", "aspx", "html", "htm", "js", "vb", "perl", "cpp", "dat", "java", "class", "css"};
-
+    public static void allowedFile(MultipartFile file) {
         int dotIndex = file.getOriginalFilename().lastIndexOf('.');
-        String extensionType = file.getOriginalFilename().substring(dotIndex + 1);
+        String ext = file.getOriginalFilename().substring(dotIndex + 1);
 
-        int lengths = notAccess.length;
-        for (int i = 0 ; i < lengths ; i++) {
-            if (extensionType.equalsIgnoreCase(notAccess[i])) {
-                log.error("#Error File Extension = {}, Contents Type : {}", extensionType, file.getContentType());
+        for (int i = 0; i < PERMITTED_EXTS.length; i++) {
+            if (!ext.equalsIgnoreCase(PERMITTED_EXTS[i])) {
+                log.error("#Error File Extension = {}, Contents Type : {}", ext, file.getContentType());
                 throw new IllegalStateException("허용된 파일 확장자가 아닙니다.");
             }
         }
@@ -32,15 +89,27 @@ public class FileUtil {
 
     /**
      * 이미지 파일일 경우 true 를 리턴한다.
-     * @param file
+     * @param uploadfile
+     * @param viewType
      * @return
      */
+    public static boolean checkImageUpload(MultipartFile uploadfile, String viewType) {
+        boolean isImage = false;
+        if (StringUtil.defaultString(viewType).equals("Image")) { // viewType 이 Image인 경우 (Space, Wiki의 에디터인 경우) 이미지 파일만 허용한다.
+            isImage = checkImageFile(uploadfile);
+            if (!isImage) {
+                throw new IllegalStateException("이미지 파일만 업로드가 허용됩니다.");
+            }
+        }
+        return isImage;
+    }
+
     public static boolean checkImageFile(MultipartFile file) {
-        final String[] contents = {"image/png", "image/jpeg", "image/jpg", "image/gif"};
+        final String[] contentsType = {"image/png", "image/jpeg", "image/jpg", "image/gif"};
         String fileContentType = file.getContentType();
-        int lengths = contents.length;
+        int lengths = contentsType.length;
         for (int i = 0 ; i < lengths ; i++) {
-            if (fileContentType.equalsIgnoreCase(contents[i])) {
+            if (fileContentType.equalsIgnoreCase(contentsType[i])) {
                 return true;
             }
         }
@@ -48,4 +117,15 @@ public class FileUtil {
     }
 
 
+    public static  void fileUpload(MultipartFile uploadfile, String path) throws IOException {
+        BufferedOutputStream stream =
+                new BufferedOutputStream(new FileOutputStream(new File(path)));
+        stream.write(uploadfile.getBytes());
+        stream.close();
+    }
+
+    public static void makeThumbnailAndUpload(MultipartFile uploadfile) {
+
+
+    }
 }
--- src/main/java/com/libqa/web/controller/CommonController.java
+++ src/main/java/com/libqa/web/controller/CommonController.java
@@ -1,9 +1,7 @@
 package com.libqa.web.controller;
 
 import com.libqa.application.dto.FileDto;
-import com.libqa.application.enums.StatusCodeEnum;
 import com.libqa.application.framework.ResponseData;
-import com.libqa.application.util.DateUtil;
 import com.libqa.application.util.FileUtil;
 import com.libqa.application.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.nio.file.Paths;
 
 /**
@@ -27,50 +23,36 @@ import java.nio.file.Paths;
 @Slf4j
 public class CommonController {
 
-    private static String SEPARATOR = "/";
-    private static String UPLOAD_TYPE_IMAGE = "image";
 
+    /**
+     * /space/ajaxUpload.hbs 파일 참조 - File input 의 onChange 시점에 임시 저장하여 view에 정보를 넘겨준다.
+     * @param uploadfile
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "/common/uploadFile", method = RequestMethod.POST)
     @ResponseBody
     public ResponseData<?> fileUpload(@RequestParam("uploadfile") MultipartFile uploadfile,
                                       HttpServletRequest request) {
-        // 허용 파일 인지를 체크
-        FileUtil.notAllowedFile(uploadfile);
 
         String viewType = request.getParameter("viewType");
         FileDto fileDto = new FileDto();
-        ResponseData data = new ResponseData();
-        String serverPath = request.getServletContext().getRealPath(SEPARATOR);
+        String serverPath = StringUtil.defaultString(request.getServletContext().getRealPath(FileUtil.SEPARATOR));
 
-        if (checkImageUpload(uploadfile, viewType, data)) return data;
+        // 허용 파일 인지를 체크
+        FileUtil.allowedFile(uploadfile);
+        // 이미지 파일일 경우 ContentType 검사
+        FileUtil.checkImageUpload(uploadfile, viewType);
 
         log.info("#### request ####");
-        log.info("# request getServletContext().getRealPath = {}", request.getServletContext().getRealPath(SEPARATOR));
+        log.info("# request getServletContext().getRealPath = {}", request.getServletContext().getRealPath(FileUtil.SEPARATOR));
         log.info("# request getServletPath = {} ", request.getServletPath());
 
         try {
             // temp/userId/yyyyMMdd/ 에 일단 저장 후 차후 userId/yyyyMMdd/파일명 으로 이동 후 삭제해야 한다.
             Integer userId = 1; // 추후 사용자 순번
-
-            String today = DateUtil.getToday();
-
-            log.info("## uploadfile : {}", uploadfile.getContentType());
-            log.info("## uploadfile : {}", uploadfile.getName());
-            log.info("## uploadfile : {}", uploadfile.getOriginalFilename());
-            log.info("## uploadfile : {}", uploadfile.getSize());
-            String[] fileType = uploadfile.getContentType().split("/");
-
-            String fileOriginalName = uploadfile.getOriginalFilename();
-            //업로드 파일 확장자 취득
-            int dotIndex = fileOriginalName.lastIndexOf('.');
-            String extendName = fileOriginalName.substring(dotIndex + 1);
-            String temp = String.valueOf(System.nanoTime());
-            //저장 파일명
-            String saveFileName = today + temp + "." + extendName.toLowerCase();
-
-
-            String localDir = "/resource/temp/" + userId;
-            String directory = serverPath + localDir;
+            String localDir = "/resource/temp/" + userId; // 임시 저장 경로
+            String directory = serverPath + localDir;  // 서버 저장 경로
 
             // 폴더 생성
             File baseDir = new File(directory);
@@ -80,25 +62,15 @@ public class CommonController {
                 log.debug("##### " + directory + "위치에 디렉토리가 존재합니다.");
             }
 
-            String filePath = Paths.get(serverPath, localDir, saveFileName).toString();
+            // 파일 정보 추출
+            fileDto = FileUtil.extractFileInfo(uploadfile, localDir);
+            fileDto.setFilePath(localDir);
+
+            String filePath = Paths.get(serverPath, localDir, fileDto.getSavedName()).toString();
             log.debug("## filePath : {}", filePath);
 
-
-            // Save the file locally
-            BufferedOutputStream stream =
-                    new BufferedOutputStream(new FileOutputStream(new File(filePath)));
-            stream.write(uploadfile.getBytes());
-            stream.close();
-
-            fileDto.setFilePath(localDir);
-            fileDto.setFileSize(uploadfile.getSize());
-            fileDto.setFileExtendType(extendName);
-            fileDto.setRealName(fileOriginalName);
-            fileDto.setSavedName(saveFileName);
-            fileDto.setUploadType(fileType[0]);       // image 여부 판단
-
-            //TODO
-            //image (profile) 일 경우 thumbnail 생성 로직 추가 되어야 함
+            // 파일 업로드 스트림
+            FileUtil.fileUpload(uploadfile, filePath);
 
             log.info("####### FILE SAVE INFO ########");
             log.info("fileDto = {}", fileDto);
@@ -109,19 +81,5 @@ public class CommonController {
         }
 
     }
-
-    private boolean checkImageUpload(MultipartFile uploadfile, String viewType, ResponseData data) {
-        if (StringUtil.defaultString(viewType).equals("Image")) { // viewType 이 Image인 경우 (Space, Wiki의 에디터인 경우) 이미지 파일만 허용한다.
-            boolean isAllowedFile = FileUtil.checkImageFile(uploadfile);
-            if (!isAllowedFile) {
-                data.setComment("이미지만 허용됩니다.");
-                data.setResultCode(StatusCodeEnum.INVALID_PARAMETER.getCode());
-                data.setData(viewType);
-                return true;
-            }
-        }
-        return false;
-    }
-
 
 }
