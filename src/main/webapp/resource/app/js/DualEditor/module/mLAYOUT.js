fc4f25e446e6f1042374cf3494366507473f4362
songanji <songanji@gmail.com>
2015-03-29 17:39:15 +0900

    UI 작업중

--- src/main/webapp/resource/app/js/DualEditor/module/mLAYOUT.js
+++ src/main/webapp/resource/app/js/DualEditor/module/mLAYOUT.js
@@ -1,9 +1,9 @@
 DualEditor.markup.LAYOUT = function(contents){
-    var pattern = /\[layout1\]([\w\W]+?)\[layout1\](\n|<br>)?\[layout2\]([\w\W]+?)\[layout2\](\n|<br>)?\[layout3\]([\w\W]+?)\[layout3\](\n|<br>)?\[layout4\]([\w\W]+?)\[layout4\]/igm;
-	contents = contents.replace(pattern, "<div class=\"row\">\r\n<div class=\"col-md-3\">\r\n$1\r\n</div>\r\n<div class=\"col-md-3\">\r\n$3\r\n</div>\r\n<div class=\"col-md-3\">\r\n$5\r\n</div>\r\n<div class=\"col-md-3\">\r\n$7\r\n</div>\r\n</div>\r\n");
-    pattern.compile(/\[layout1\]([\w\W]+?)\[layout1\](\n|<br>)?\[layout2\]([\w\W]+?)\[layout2\](\n|<br>)?\[layout3\]([\w\W]+?)\[layout3\]/igm);
-    contents = contents.replace(pattern, "<div class=\"row\">\r\n<div class=\"col-md-4\">\r\n$1\r\n</div>\r\n<div class=\"col-md-4\">\r\n$3\r\n</div>\r\n<div class=\"col-md-4\">\r\n$5\r\n</div>\r\n</div>\r\n");
-    pattern.compile(/\[layout1\]([\w\W]+?)\[layout1\](\n|<br>)?\[layout2\]([\w\W]+?)\[layout2\]/igm);
-    contents = contents.replace(pattern, "<div class=\"row\">\r\n<div class=\"col-md-6\">\r\n$1\r\n</div>\r\n<div class=\"col-md-6\">\r\n$3\r\n</div>\r\n</div>\r\n");
-	return contents;
+    var pattern = /^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm;
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-3\">$1</div><div class=\"col-md-3\">$2</div><div class=\"col-md-3\">$3</div><div class=\"col-md-3\">$4</div></div>");
+    pattern.compile(/^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-4\">$1</div><div class=\"col-md-4\">$2</div><div class=\"col-md-4\">$3</div></div>");
+    pattern.compile(/^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-6\">$1</div><div class=\"col-md-6\">$2</div></div>");
+    return contents;
 }
