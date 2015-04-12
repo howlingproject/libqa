217fd0e41953fc0764b19ad2e7fe8f411643a2f6
songanji <songanji@gmail.com>
2015-04-12 16:19:46 +0900

    wiki 메인개발

--- src/main/java/com/libqa/web/service/WikiServiceImpl.java
+++ src/main/java/com/libqa/web/service/WikiServiceImpl.java
@@ -5,7 +5,11 @@ import com.libqa.web.domain.Wiki;
 import com.libqa.web.repository.WikiRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by songanji on 2015. 3. 1..
@@ -39,6 +43,29 @@ public class WikiServiceImpl implements WikiService {
     @Override
     public Wiki findById(Integer wikiId) {
         return wikiRepository.findOne(wikiId);
+    }
+
+    @Override
+    public List<Wiki> findByAllWiki(int startIdx, int endIdx) {
+        PageRequest pageRequest = new PageRequest(startIdx, endIdx, new Sort(new Sort.Order(Sort.Direction.DESC, "insertDate")));
+        return wikiRepository.findAll(pageRequest).getContent();
+    }
+
+    @Override
+    public List<Wiki> findByBestWiki(int startIdx, int endIdx) {
+        return null;
+    }
+
+    @Override
+    public List<Wiki> findByRecentWiki(int userId, int startIdx, int endIdx) {
+        PageRequest pageRequest = new PageRequest(startIdx, endIdx,
+                new Sort(new Sort.Order(Sort.Direction.DESC, "userId")
+                ,new Sort.Order(Sort.Direction.DESC, "insertDate")
+                )
+
+        );
+
+        return wikiRepository.findAll(pageRequest).getContent();
     }
 
 
