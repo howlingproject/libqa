3c1abcf84c0411fdf9d16f3421a241521766c8e8
sjune <alivejune@gmail.com>
2015-06-21 15:05:00 +0900

    added xif helper

--- src/main/webapp/resource/app/js/helpers/helper.js
+++ src/main/webapp/resource/app/js/helpers/helper.js
@@ -3,3 +3,26 @@ Handlebars.registerHelper('nl2br', function(text) {
     text = text.toString().replace(/(\r\n|\n|\r)/gm, '<br>');
     return new Handlebars.SafeString(text);
 });
+
+Handlebars.registerHelper('xif', function (v1, operator, v2, options) {
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
