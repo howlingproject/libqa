922ffe39ebeb070f7f4e3d7597c78d9bdde45b0a
yongpwi <seeds.pear.jr@gmail.com>
2015-12-20 18:41:41 +0900

    qa main, my list 수정

--- src/main/webapp/resource/app/js/helpers/helper.js
+++ src/main/webapp/resource/app/js/helpers/helper.js
@@ -32,3 +32,28 @@ Handlebars.registerHelper('keywordLink', function(keywordType, keywordName) {
     }
     return '#';
 });
+
+Handlebars.registerHelper('keywordBadge', function(keywordName){
+    var keywordLength = Handlebars.helpers.length(keywordName);
+
+    switch (keywordLength) {
+        case 1:
+            return "<span class=\"label label-primary\">" + keywordName +"</span>";
+        case 2:
+            return "<span class=\"label label-success\">" + keywordName +"</span>";
+        case 3:
+            return "<span class=\"label label-info\">" + keywordName +"</span>";
+        case 4:
+            return "<span class=\"label label-warning\">" + keywordName +"</span>";
+        case 5:
+            return "<span class=\"label label-danger\">" + keywordName +"</span>";
+        case 6:
+            return "<span class=\"label label-tag\">" + keywordName +"</span>";
+        case 7:
+            return "<span class=\"label label-focus\">" + keywordName +"</span>";
+        case 8:
+            return "<span class=\"label label-alert\">" + keywordName +"</span>";
+        default :
+            return "<span class=\"label label-default\">" + keywordName +"</span>";
+    }
+});
