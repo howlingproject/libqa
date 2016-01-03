4cd09ecd29d02de4b2f72b5683277bc5360e9132
sjune <alivejune@gmail.com>
2016-01-03 16:37:07 +0900

    깨지는 테스트 수정

--- src/main/java/com/libqa/web/view/feed/DisplayDateParser.java
+++ src/main/java/com/libqa/web/view/feed/DisplayDateParser.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class DisplayDateParser {
     private static final String FEED_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
-    static String parseForFeed(Date date) {
+    public static String parseForFeed(Date date) {
         return new SimpleDateFormat(FEED_DATE_FORMAT).format(date);
     }
 }
