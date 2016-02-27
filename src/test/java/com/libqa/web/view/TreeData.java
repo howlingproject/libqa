0851f9b437474839fa9a89add845152c68ff8c0c
yionlee <performizer@gmail.com>
2016-02-27 16:01:13 +0900

    트리메뉴 데이터 테스트 2

--- src/test/java/com/libqa/web/view/TreeData.java
+++ src/test/java/com/libqa/web/view/TreeData.java
@@ -13,70 +13,106 @@ import java.util.List;
  */
 public class TreeData {
 
+    @Test
+    public void makeSubTree() {
+        List<WikiTree> wikiList = setData();
+        System.out.println(" wiki List : " + wikiList.size());
+
+        List<WikiTree> newList = wikiList.subList(1, wikiList.size());
+        System.out.println("new List : " + newList);
+        System.out.println("new List : " + newList.size());
+
+
+        List<WikiTree> newList1 = newList.subList(1, newList.size());
+        System.out.println("new List1 : " + newList1);
+        System.out.println("new List1 : " + newList1.size());
+
+    }
+
 
     @Test
     public void makeTree() {
-        List<WikiTree> wikiList = setData();
+        List<WikiTree> wikiList = simpleData();
         System.out.println("## wikiList : " + wikiList);
-        String result = recursiveTree(wikiList);
+        String result = recursiveTree(wikiList, wikiList.get(0).getParentsId());
         System.out.println("### result : \n" + result);
     }
 
-
-    public String recursiveTree(List<WikiTree> wikiList) {
+    public String recursiveTree(List<WikiTree> wikiList, int parentId) {
         String ret = "<ul>";
         String sub = "";
-        int root = 0;
-        int groupId = 0;
+
         for (int i = 0; i < wikiList.size(); i++) {
+            if (parentId == wikiList.get(i).getParentsId()) {
+                ret += "<li>" + wikiList.get(i).getTitle();
 
-            System.out.println("## depth [" + i + "] = " + wikiList.get(i).getDepthIdx() + " : " + wikiList.get(i).getTitle());
-            if (root == wikiList.get(i).getDepthIdx()) {
-                ret += "<li>" + wikiList.get(i).getTitle() ;
-            }
-
-            if (groupId == wikiList.get(i).getGroupIdx() && groupId != 0) {
-                ret += "<ul><li>" + wikiList.get(i).getTitle() + "</li></ul>\n";
-            }
-
-            if (wikiList.get(i).getOrderIdx() == wikiList.get(i).getMaxRows()) {
-                ret += "</li>\n";
-            }
-
-            groupId = wikiList.get(i).getGroupIdx();
-        }
-
-        return ret+"</ul>";
-    }
+                if (wikiList.size() > 1) {
+                    List<WikiTree> newList = wikiList.subList(1, wikiList.size());
+                    System.out.println("New List : = " + newList);
+                    System.out.println("New List Size : = " + newList.size());
+                    System.out.println("newList.get(0).getParentsId() = " + newList.get(0).getParentsId());
 
 
-
-    public String recursived(List<WikiTree> categories , int parent)
-    {
-        String ret = "<ul>";
-        String sub ="";
-
-        try {
-            //String xx=categories.getString("sap_code");
-            for(int i=0;i<categories.size();i++)
-            {
-                if(categories.get(i).getParentsId() == parent)
-                {
-                    ret += "<li><a href=='#'>"+ categories.get(i).getTitle() + "</a>";
-                    List newList = categories.subList(0,categories.size() -1);
-                    sub = recursived(newList ,categories.get(i).getParentsId());
-                    if(sub != "<ul></ul>")
+                    sub = recursiveTree(newList, newList.get(0).getParentsId());
+                    System.out.println("@@@@@@ sub = " + sub);
+                    if (sub != "<ul></ul>")
                         ret += sub;
+
                     ret += "</li>";
                 }
             }
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
 
+        }
         return ret+"</ul>";
     }
 
+
+    public List<WikiTree> simpleData() {
+        List<WikiTree> wikiList = new ArrayList<>();
+
+        WikiTree wiki1 = new WikiTree();
+        wiki1.setGroupIdx(126);
+        wiki1.setWikiId(126);
+        wiki1.setParentsId(126);
+        wiki1.setDepthIdx(0);
+        wiki1.setOrderIdx(0);
+        wiki1.setTitle("첫번째 위키");
+        wiki1.setMaxRows(4);
+        wikiList.add(wiki1);
+
+        WikiTree wiki2 = new WikiTree();
+        wiki2.setGroupIdx(126);
+        wiki2.setWikiId(128);
+        wiki2.setParentsId(126);
+        wiki2.setDepthIdx(1);
+        wiki2.setOrderIdx(1);
+        wiki2.setTitle("첫번째 위키의 첫번째자식");
+        wiki2.setMaxRows(4);
+        wikiList.add(wiki2);
+
+        WikiTree wiki3 = new WikiTree();
+        wiki3.setGroupIdx(126);
+        wiki3.setWikiId(129);
+        wiki3.setParentsId(128);
+        wiki3.setDepthIdx(2);
+        wiki3.setOrderIdx(2);
+        wiki3.setTitle("첫번째 위키의 자식위키의 자식");
+        wiki3.setMaxRows(4);
+        wikiList.add(wiki3);
+
+        WikiTree wiki4 = new WikiTree();
+        wiki4.setGroupIdx(126);
+        wiki4.setWikiId(133);
+        wiki4.setParentsId(126);
+        wiki4.setDepthIdx(1);
+        wiki4.setOrderIdx(3);
+        wiki4.setTitle("첫번째 위키의 두번째자식");
+        wiki4.setMaxRows(4);
+        wikiList.add(wiki4);
+
+
+        return wikiList;
+    }
 
     public List<WikiTree> setData() {
         List<WikiTree> wikiList = new ArrayList<>();
@@ -150,9 +186,6 @@ public class TreeData {
         wiki7.setTitle("1 - 1 - 2 - 2");
         wiki7.setMaxRows(6);
         wikiList.add(wiki7);
-
-
-
 
         return wikiList;
     }
