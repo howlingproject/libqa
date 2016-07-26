f252b69d020b6992a0b67b3314c679e74925df1f
songanji <songanji@gmail.com>
2016-07-27 01:42:04 +0900

    위키에디터 - UI 수정

--- src/main/webapp/resource/app/js/DualEditor/module/mLAYOUT.js
+++ src/main/webapp/resource/app/js/DualEditor/module/mLAYOUT.js
@@ -1,9 +1,10 @@
 DualEditor.markup.LAYOUT = function(contents){
-    var pattern = /\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm;
-    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-3\">$1</div><div class=\"col-md-3\">$2</div><div class=\"col-md-3\">$3</div><div class=\"col-md-3\">$4</div></div>");
-    pattern.compile(/\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
-    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-4\">$1</div><div class=\"col-md-4\">$2</div><div class=\"col-md-4\">$3</div></div>");
-    pattern.compile(/\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
-    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-6\">$1</div><div class=\"col-md-6\">$2</div></div>");
+
+    var pattern = /(<br>)?\[layout\](.*?)\[layout\]\n(<br>)?\[layout\](.*?)\[layout\]\n(<br>)?\[layout\](.*?)\[layout\]\n(<br>)?\[layout\](.*?)\[layout\]/igm;
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-3\">$2</div><div class=\"col-md-3\">$4</div><div class=\"col-md-3\">$6</div><div class=\"col-md-3\">$8</div></div>");
+    pattern.compile(/(<br>)?\[layout\](.*?)\[layout\]\n(<br>)?\[layout\](.*?)\[layout\]\n(<br>)?\[layout\](.*?)\[layout\]/igm);
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-4\">$2</div><div class=\"col-md-4\">$4</div><div class=\"col-md-4\">$6</div></div>");
+    pattern.compile(/(<br>)?\[layout\](.*?)\[layout\]\n(<br>)?\[layout\](.*?)\[layout\]/igm);
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-6\">$2</div><div class=\"col-md-6\">$4</div></div>");
     return contents;
 };
